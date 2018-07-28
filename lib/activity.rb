c924c24f53458c85fd07a997fc993642f8f803e4
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-07-28 14:07:51 -0600

    completes iteration 1

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,30 @@
+require 'pry'
 class Activity
+  attr_reader :name,
+              :participants
+
+  def initialize(name)
+    @name = name
+    @participants = []
+    @total_fees = []
+
+  end
+
+  def add_participant(participant)
+    @participants << participant
+  end
+
+  def total_fees
+
+    @participants.map do |participant|
+      @total_fees << participant["fees"]
+    end
+      @total_fees.sum
+  end
+
+
+
+
+
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,45 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_activity_exists
+    activity = Activity.new("Soccer")
+    assert_instance_of Activity, activity
+  end
+
+  def test_activity_has_name
+    activity = Activity.new("Soccer")
+    assert_equal "Soccer", activity.name
+  end
+
+  def test_there_is_a_list_of_participants
+    activity = Activity.new("Soccer")
+    assert_equal [], activity.participants
+  end
+
+  def test_it_can_add_a_participant
+    activity = Activity.new("Soccer")
+    activity.add_participant({"name" => "Todd", "fees" => 5})
+    assert_equal "Todd", activity.participants[0]["name"]
+     assert_equal 5,      activity.participants[0]["fees"]
+  end
+
+  def test_it_can_add_multiple_participants
+    activity = Activity.new("Soccer")
+    activity.add_participant({"name" => "Todd", "fees" => 5})
+    activity.add_participant({"name" => "Molly", "fees" => 7})
+    assert_equal "Molly", activity.participants[1]["name"]
+    assert_equal 7,      activity.participants[1]["fees"]
+  end
+
+  def test_it_can_add_up_fees_per_activity
+    activity = Activity.new("Soccer")
+    activity.add_participant({"name" => "Todd", "fees" => 5})
+    activity.add_participant({"name" => "Molly", "fees" => 7})
+
+    assert_equal 12, activity.total_fees
+  end
+
+
+
 end
